a0e5839d6addfa5278f91475150bc60f53a15841
Radu Mac <radulepa@gmail.com>
2025-05-22 10:47:56 +0300

    nearform interview

--- server/src/app.js
+++ server/src/app.js
@@ -10,8 +10,81 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use(express.json());
-app.use(routes);
+const vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', "I", 'O', "U"]
+
+function applyStyling(word, style) {
+  switch (style) {
+    case "uppercase":
+      return word.toUpperCase();
+
+    case "capital":
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+      
+    default:
+      return word.toLowerCase();
+
+  }
+}
+
+
+function getStyle(word) {
+  if (word === word.toUpperCase())
+    return "uppercase";
+
+  if (word[0] === word[0].toUpperCase())
+    return "capital"
+
+  return "normal";
+}
+export function createWording(word) {
+
+  const firstLetter = word[0];
+  if (!vowel.includes(firstLetter)) {
+
+    for (let i = 0; i < word.length; i++) {
+
+      if (vowel.includes(word[i])) {
+        const prefix = word.slice(0, i);
+        const suffix = word.slice(i);
+        const wordFinal = suffix + prefix + 'ay';
+        const style = getStyle(word);
+        console.log("wordF", wordFinal)
+        console.log('style', style)
+        const correct = applyStyling(wordFinal, style);
+        console.log('correct', correct)
+        return correct;
+        // return suffix + prefix + 'ay';
+      }
+
+    }
+
+  }
+  else {
+    return word + "yay";
+  }
+
+}
+
+
+
+// pleased to meet you
+// easedplay otay eetmay ouyay
+
+
+
+export function fixSentence(sentence) {
+  return sentence.split(' ').map(word => createWording(word)).join(" ");
+}
+
+// console.log(createWording('pig'))
+// console.log(createWording('latin'))
+// console.log(createWording('smile'))
+// console.log(createWording('floor'))
+// console.log(createWording('egg'))
+// console.log(createWording('explain'))
+
+console.log(fixSentence("A tree whose elements have at most two children is called a BINARY TREE"))
+
 
 app.get('/', (req, res) => {
   res.send('Server is up and running!');
--- server/src/tests/app.test.js
+++ server/src/tests/app.test.js
@@ -1,19 +1,85 @@
 import request from 'supertest';
-import app from '../app.js';
+import app, { fixSentence } from '../app.js';
+import { createWording } from '../app.js';
 
-describe('GET /', () => {
-  it('should return server running message', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Server is up and running!');
+// "pig" = "igpay"
+// "latin" = "atinlay"
+// "smile" = "ilesmay"
+// "floor"= "oorflay"
+// "string" = "ingstray"
+
+describe('test word', () => {
+  it('it shoud return corect words first concons', async () => {
+    const result = createWording('pig');
+    expect(result).toBe('igpay');
   });
-});
 
-describe('GET /api/health', () => {
-    it('should return server health message', async () => {
-      const res = await request(app).get('/api/health');
-      expect(res.statusCode).toBe(200);
-      expect(res.body.status).toBe('OK');
-      expect(res.body.message).toBe('Server is running!');
-    });
+  it('it shoud return corect words first concons', async () => {
+    const result = createWording('floor');
+    expect(result).toBe('oorflay');
+  });
+
+  it('it shoud return corect words first wovel', async () => {
+    const result = createWording('egg');
+    expect(result).toBe('eggyay');
+  });
+
+  // new ones
+  it('it shoud return corect words first concons', async () => {
+    const result = createWording('dig');
+    expect(result).toBe('igday');
+  });
+
+  it('it shoud return corect words first concons', async () => {
+    const result = createWording('door');
+    expect(result).toBe('oorday');
+  });
+
+  it('it shoud return corect words first wovel', async () => {
+    const result = createWording('iooo');
+    expect(result).toBe('ioooyay');
+  });
+
+
+  it('it shoud return corect sentence based on tes', async () => {
+    const result = fixSentence('pleased to meet you');
+    expect(result).toBe('easedplay otay eetmay ouyay');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('how much does it cost');
+    expect(result).toBe('owhay uchmay oesday ityay ostcay');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('i would rather be at the beach');
+    expect(result).toBe('iyay ouldway atherray ebay atyay ethay eachbay');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('a tree whose elements have at most two children is called a binary tree');
+    expect(result).toBe('ayay eetray osewhay elementsyay avehay atyay ostmay otway ildrenchay isyay alledcay ayay inarybay eetray');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('Pleased to meet you');
+    expect(result).toBe('Easedplay otay eetmay ouyay');
+  });
+
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('Do you speak Pig Latin');
+    expect(result).toBe('Oday ouyay eakspay Igpay Atinlay');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('Time flies when you are having fun');
+    expect(result).toBe('Imetay iesflay enwhay ouyay areyay avinghay unfay');
+  });
+
+  it('it shoud return corect sentence', async () => {
+    const result = fixSentence('A tree whose elements have at most two children is called a BINARY TREE');
+    expect(result).toBe('Ayay eetray osewhay elementsyay avehay atyay ostmay otway ildrenchay isyay alledcay ayay INARYBAY EETRAY');
+  });
+
 });
